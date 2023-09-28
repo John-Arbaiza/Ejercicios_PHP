494308138cb4c526ace560964442836749fb0e93
John Elvis Arbaiza Velásquez <94189760+John-Arbaiza@users.noreply.github.com>
2023-09-27 22:16:54 -0600

    Practica: Uso del FrontController
    Primeros pasos en ver el funbcionamiento del frontController

---
+++ Repaso/frontController/objects/config/configControllers.php
@@ -0,0 +1,13 @@
+<?php
+
+//Definimos una constante que representa la URL base de nuestro proyecto
+define ('BASE_DIR', "http://localhost/Repaso/frontController/objects/");
+
+//Definimos la constante que especifica la carpeta donde se alamcenan los controladores
+define ('CONTROLLERS_DIR', "controllers/");
+
+//Constante que establece el controlador predeterminado
+define ('DEFAULT_CONTROLLER', "Home");
+
+//Constante que establece la accion predertimanada en el controlador predeterminado
+define ('DEFAULT_ACTION', "showHome");
