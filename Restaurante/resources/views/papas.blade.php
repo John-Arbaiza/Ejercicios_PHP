dba9842d860d27343fccff9a71b9c956a3c29098
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2023-11-09 16:44:03 -0600

    Practica 1 Laravel

---
+++ Restaurante/resources/views/papas.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Papas</title>
+</head>
+
+<style>
+        body{
+        background-color:#F1F6F9;
+    }
+
+    h1{
+        text-align:center;
+        color:#394867;
+    }
+
+    p{
+        text-align:center;
+        font-size:20px;
+    }
+        button{
+        margin:20px;
+        width: 120px;
+        height: 38px;
+        background-color:#212A3E;
+        border: 2px solid #9BA4B5;
+        border-radius:4px;
+    }
+    a{
+      text-decoration:none;
+      color:white;
+      font-weight:bolder;
+    }
+
+    img{
+        width: 500px;
+        height:300px;
+    }
+</style>
+<body>
+    <h1>Papas Fritas</h1>
+
+    <div class="pupas" style="display:flex; justify-content:center;">
+       <img src="https://scm-assets.constant.co/scm/unilever/86c9fcb727c4d90deffba18b2593a9cf/8ef558a9-6ce0-4d26-9ef2-ca79a781fd67.png" alt="Papas">
+    </div>
+    <br>
+    <p>Las papas fritas, también conocidas como papas a la francesa, son un plato de patatas que se cocinan mediante fritura hasta que queden doradas y crujientes. <br>
+      Pueden consumirse solas o condimentadas con sal, kétchup, mayonesa u otras salsas     
+    </p>
+
+     <h2 style="text-align:center;">Preparación</h2>
+     <h3 style="text-align:center;">los pasos para preparar unas ricas papas fritas son los siguientes:</h3>
+    <div class="preparacion" style="display:flex; flex-direction:column; align-items:center;">
+        <ul>
+            <li>Pelar las papas y córtalas en tiras finas</li>
+            <li> Calentar el aceite en una sartén grande sobre fuego medio-alto. </li>
+            <li>agregar una parte de las papas y cocinar, moviendo de vez en cuando, hasta que las papitas estén doradas</li>
+            <li>Prepar la salsa con la que combinar las papas</li>
+            <li>Retírar las papas del aceite y ponerlas en un plato cubierto con varias capas de servilletas de papel para que escurra el aceite.</li>
+            <li>Sirvimos las papitas fritas con las salsas.</li>
+         </ul>
+
+         <button>
+           <a href="/restaurante">Menu</a>
+         </button>
+
+    </div> 
+</body>
+</html>
---
+++ Restaurante/resources/views/pupusas.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pupusas</title>
+</head>
+
+<style>
+        body{
+        background-color:#F1F6F9;
+    }
+
+    h1{
+        text-align:center;
+        color:#394867;
+    }
+
+    p{
+        text-align:center;
+        font-size:20px;
+    }
+        button{
+        margin:20px;
+        width: 120px;
+        height: 38px;
+        background-color:#212A3E;
+        border: 2px solid #9BA4B5;
+        border-radius:4px;
+    }
+    a{
+      text-decoration:none;
+      color:white;
+      font-weight:bolder;
+    }
+</style>
+<body>
+    <h1>Pupusas el diez</h1>
+
+    <div class="pupas" style="display:flex; justify-content:center;">
+       <img src="https://www.inmsol.com/wp-content/uploads/2016/05/o.jpg" alt="PUPUSAS">
+    </div>
+    <br>
+    <p>La pupusa es una tortilla gruesa a base de masa de maíz o de arroz rellena con uno o más ingredientes,
+         como queso, chicharrón, ayote, frijoles refritos, loroco, etc.​ <br><br>      
+    </p>
+
+     <h2 style="text-align:center;">Preparación</h2>
+     <h3 style="text-align:center;">los pasos para preparar las ricas pupusas son los siguientes:</h3>
+    <div class="preparacion" style="display:flex; flex-direction:column; align-items:center;">
+        <ul>
+            <li>Mezclar la harina de maíz o arroz , con agua hasta formar la masa.</li>
+            <li>Preparar el quesillo o queso mozarella hasta que no queden grumos.</li>
+            <li>Hacer una tortilla, colocar en el centro el quesillo  junto con los demas ingredientes, palmearlo hasta formar una tortilla mediana.</li>
+            <li>Colocarlas en una plancha cocinar hasta que este cocida y salga el queso de la tortilla.</li>
+            <li>Servir junto con el curtido y las salsas.</li>
+         </ul>
+
+         <button>
+           <a href="/restaurante">Menu</a>
+         </button>
+
+    </div> 
+</body>
+</html>
